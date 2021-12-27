67d89c4c18021039fe27303ee14268ba25b6e202
Joscha Loos <j.loos@posteo.net>
2021-12-27 23:47:35 +0100

    * cargo2nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,64 @@
 {
   inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/master";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-21.05";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        naersk-lib = naersk.lib."${system}";
-      in
-        rec {
-          # `nix build`
-          packages.tyt = naersk-lib.buildPackage {
-            pname = "tyt";
-            root = ./.;
-          };
-          defaultPackage = packages.tyt;
+  outputs = { self, nixpkgs, cargo2nix, flake-utils, rust-overlay, ... }:
 
-          # `nix run`
-          apps.tyt = flake-utils.lib.mkApp {
-            drv = packages.tyt;
-          };
-          defaultApp = apps.tyt;
+    # Build the output set for each default system and map system sets into
+    # attributes, resulting in paths such as:
+    # nix build .#packages.x86_64-linux.<name>
+    flake-utils.lib.eachDefaultSystem (system:
 
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [ rustc cargo ];
-          };
-        }
+      # let-in expressions, very similar to Rust's let bindings.  These names
+      # are used to express the output but not themselves paths in the output.
+      let
+
+        # create nixpkgs that contains rustBuilder from cargo2nix overlay
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import "${cargo2nix}/overlay")
+                      rust-overlay.overlay];
+        };
+
+        # create the workspace & dependencies package set
+        rustPkgs = pkgs.rustBuilder.makePackageSet' {
+          rustChannel = "1.56.1";
+          packageFun = import ./Cargo.nix;
+          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              name = "clipboard";
+              overrideAttrs = drv: {
+                propagatedNativeBuildInputs = drv.propagatedNativeBuildInputs or [ ] ++ [
+                  pkgs.xorg.libxcb.dev
+                  # pkgs.libX11
+                ];
+              };
+            })
+          ];
+        };
+
+      in rec {
+        # this is the output (recursive) set (expressed for each system)
+
+        # the packages in `nix build .#packages.<system>.<name>`
+        packages = {
+          # nix build .#bigger-project
+          # nix build .#packages.x86_64-linux.bigger-project
+          tyt = (rustPkgs.workspace.tyt {}).bin;
+        };
+
+        # nix build
+        defaultPackage = packages.tyt;
+
+        # nix run
+        defaultApp = packages.tyt;
+      }
     );
 }
+
